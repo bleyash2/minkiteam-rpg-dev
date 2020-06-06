4a458e67192b99ff13e1c3c6a3c37ce0b473c6ab
bleyash2 <ryangash2@gmail.com>
2020-06-06 17:16:01 +1000

    Modified Scene, Adds Sprites and Animations to Play

---
+++ Assets/Scripts/ClearSight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionDistance
+{
+    public Vector3 _direction;
+    public float _distance;
+    public DirectionDistance(Vector3 direction, float distance)
+    {
+        this._direction = direction;
+        this._distance = distance;
+    }
+}
+
+public class ClearSight : MonoBehaviour
+{
+    public float DistanceToPlayer = 5.0f;
+    public Transform PlayerPos;
+
+
+    DirectionDistance getDirectionFromCameraToPlayer()
+    {
+        Vector3 heading = transform.position - PlayerPos.position;
+        float distance = heading.magnitude;
+        Vector3 direction = heading / distance;
+        return new DirectionDistance(direction, distance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit[] hits;
+        Debug.DrawRay(transform.position, transform.forward, Color.green, 20);
+        hits = Physics.RaycastAll(transform.position, transform.forward, getDirectionFromCameraToPlayer()._distance);
+        Debug.Log(hits.Length.ToString());
+        foreach (RaycastHit hit in hits)
+        {
+            Renderer R = hit.collider.GetComponent<Renderer>();
+            if (R == null)
+            {
+                continue;
+            }
+            AutoTransparent AT = R.GetComponent<AutoTransparent>();
+            if (AT == null)
+            {
+                continue;
+            }
+            AT.BeTransparent();
+        }
+    }
+}
--- Assets/Scripts/DirectionalMovement.cs
+++ Assets/Scripts/DirectionalMovement.cs
@@ -7,16 +7,24 @@ public class DirectionalMovement : MonoBehaviour
     public float movementSpeed;
     public Rigidbody rb;
     Vector3 movement;
+
+    public Animator animator;
     void handleMovement()
     {
         movement.x = -Input.GetAxisRaw("Horizontal");
         movement.z = -Input.GetAxisRaw("Vertical");
         movement.Normalize();
+        //Set Ani Variables
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void Update()
     {
         handleMovement();
+
+
     }
 
     // Update is called once per frame
