b62733bd44947b2305e6bca4b47a9cdc421dfe8b
bleyash2 <ryangash2@gmail.com>
2020-06-06 15:33:32 +1000

    Adds Camera Following and Basic Movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 cameraOffset;
+    public float smoothingSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothingSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/DirectionalMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody rb;
+    Vector3 movement;
+    void handleMovement()
+    {
+        movement.x = -Input.GetAxisRaw("Horizontal");
+        movement.z = -Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+    }
+
+    void Update()
+    {
+        handleMovement();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
