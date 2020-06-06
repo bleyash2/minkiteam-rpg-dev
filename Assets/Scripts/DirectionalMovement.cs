45365b03c88b801b32843290c9a470430210dfa2
Ryan Ashworth <ryangash2@gmail.com>
2020-06-06 00:40:24 -0700

    Adds Scenes for Testing (#2)
    * Adds Camera Following and Basic Movement
    
    * Modified Scene, Adds Sprites and Animations to Play
    
    * Adds Starter for Battle Screen

---
+++ Assets/Scripts/DirectionalMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody rb;
+    Vector3 movement;
+
+    public Animator animator;
+    void handleMovement()
+    {
+        movement.x = -Input.GetAxisRaw("Horizontal");
+        movement.z = -Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+        //Set Ani Variables
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void Update()
+    {
+        handleMovement();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
